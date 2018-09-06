003b86ed0154c94c2e23b693fbf2c156902db847
天 谢 <jsjtxietian@outlook.com>
2018-09-06 20:17:47 +0800

    first

---
+++ Assets/Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Config : MonoBehaviour
+{
+    public List<string> RFIDs = new List<string>();
+
+    void Start()
+    {
+        string path = System.IO.Path.Combine(Application.streamingAssetsPath, "Num.txt");
+
+        using (StreamReader sr = new StreamReader(path))
+        {
+            string line;
+
+            // 从文件读取并显示行，直到文件的末尾 
+            while ((line = sr.ReadLine()) != null)
+            {
+                RFIDs.Add(line);
+            }
+        }
+
+    }
+
+}
---
+++ Assets/RFIDReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RFIDReader : MonoBehaviour
+{
+    private Config Config;
+    private List<char> CurrentInputs = new List<char>();
+    private char currentOne;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Config = gameObject.GetComponent<Config>();
+	}
+
+    void OnGUI()
+    {
+        if (Input.anyKeyDown)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                var key = e.keyCode;
+                Debug.Log(key);
+                if (key == KeyCode.Return)
+                {
+                    string s = new string(CurrentInputs.ToArray());
+                    CurrentInputs.Clear();
+                    currentOne = Char.MinValue;
+                    AddRFID(s);
+                }
+                else if (key == KeyCode.None)
+                {
+                    CurrentInputs.Add(currentOne);
+                }
+                else
+                {
+                    currentOne = key.ToString().ToCharArray()[key.ToString().Length - 1];
+                }
+            }
+        }
+    }
+
+    private void AddRFID(string rfid)
+    {
+        foreach (var prebuild in Config.RFIDs)
+        {
+            if (rfid.Equals(prebuild))
+            {
+                Debug.Log("success" + rfid);
+            }
+        }
+    }
+}
