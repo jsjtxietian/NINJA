64eb88d7f6451155252334d3f076baeb646fa1df
天 谢 <jsjtxietian@outlook.com>
2018-09-07 01:24:38 +0800

    add some ui & touch input

---
+++ Assets/FollowTouch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTouch : MonoBehaviour
+{
+    public int index;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.touchCount > index)
+	    {
+	        gameObject.GetComponent<RectTransform>().position = Input.touches[index].position;
+	    }
+	}
+}
